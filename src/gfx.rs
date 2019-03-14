82edbd05330102ad27e31d1ef2b421c2198ed21e
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-03-14 15:30:56 +0100

    Cleanup a bit in `gfx` module

--- src/gfx.rs
+++ src/gfx.rs
@@ -1,10 +1,12 @@
+//! Everything related to visualising the Sim game state.
+
 use std::{
     sync::mpsc::Receiver,
 };
 use quicksilver::{
-    Result,
+    Error,
     geom::{Circle, Line},
-    graphics::{Background::Col, Color},
+    graphics::{Background, Color},
     lifecycle::{State, Window},
 };
 
@@ -13,24 +15,35 @@ use crate::{
     GameState, EdgeId, EdgeState,
 };
 
+/// A `quicksilver` state that displays a Sim game state.
 pub(crate) struct SimDisplay {
     pub(crate) last_state: GameState,
     pub(crate) new_states: Receiver<GameState>,
 }
 
+
+const COLOR_GREY: Color = Color { r: 0.6, g: 0.6,  b: 0.6,  a: 1.0 };
+const COLOR_RED: Color = Color {  r: 0.9, g: 0.29, b: 0.23, a: 1.0 };
+const COLOR_BLUE: Color = Color { r: 0.3, g: 0.49, b: 1.0,  a: 1.0 };
+const POINT_COLOR: Color = Color::WHITE;
+const BACKGROUND_COLOR: Color = Color::BLACK;
+
+
 impl State for SimDisplay {
-    fn new() -> Result<SimDisplay> {
-        unimplemented!()
+    fn new() -> Result<SimDisplay, Error> {
+        panic!(
+            "Called `SimDisplay::new`: this method must not be called \
+                (use `run_with` to create this state)"
+        );
     }
 
-    fn draw(&mut self, window: &mut Window) -> Result<()> {
+    fn draw(&mut self, window: &mut Window) -> Result<(), Error> {
         while let Ok(state) = self.new_states.try_recv() {
             self.last_state = state;
         }
 
-        window.clear(Color::BLACK)?;
+        window.clear(BACKGROUND_COLOR)?;
 
-        let point_color = Col(Color::WHITE);
         let points = [
             (350, 250),
             (650, 250),
@@ -43,20 +56,23 @@ impl State for SimDisplay {
         // Draw all edges
         for e in (0..15).map(EdgeId::new) {
             let (color, width) = match self.last_state.edge_state(e) {
-                EdgeState::None => (Col(Color { r: 0.6, g: 0.6, b: 0.6, a: 1.0 }), 1.5),
-                EdgeState::Red => (Col(Color { r: 0.9, g: 0.29, b: 0.23, a: 1.0 }), 4.0),
-                EdgeState::Blue => (Col(Color { r: 0.3, g: 0.49, b: 1.0, a: 1.0 }), 6.0),
+                EdgeState::None => (COLOR_GREY, 1.5),
+                EdgeState::Red => (COLOR_RED, 4.0),
+                EdgeState::Blue => (COLOR_BLUE, 6.0),
             };
 
             let (va, vb) = e.endpoints();
             let pa = points[va.0 as usize];
             let pb = points[vb.0 as usize];
-            window.draw(&Line::new(pa, pb).with_thickness(width), color);
+            window.draw(
+                &Line::new(pa, pb).with_thickness(width),
+                Background::Col(color),
+            );
         }
 
         // Draw all points
         for &p in &points {
-            window.draw(&Circle::new(p, 10), point_color);
+            window.draw(&Circle::new(p, 10), Background::Col(POINT_COLOR));
         }
 
         Ok(())
