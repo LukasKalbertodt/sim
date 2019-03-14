bf20b9f65112e3e293a63f1911855dc6dfe5c4c6
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-03-14 20:20:53 +0100

    Properly validate players given via command line

--- src/main.rs
+++ src/main.rs
@@ -17,33 +17,63 @@ mod player;
 
 fn main() {
     let opt = Opt::from_args();
-
-    let player_red = get_player(&opt.player_red, EdgeState::Red);
-    let player_blue = get_player(&opt.player_blue, EdgeState::Blue);
+    let player_red = opt.player_red.computer_player();
+    let player_blue = opt.player_blue.computer_player();
 
     run_with("Sim", Vector::new(1000, 1000), Settings::default(), || {
         Ok(GuiGame::new(player_red, player_blue))
     });
 }
 
-fn get_player(label: &str, color: EdgeState) -> Option<Box<dyn Player>> {
-    match label {
-        "human" => None,
-        "random" => Some(Box::new(Random::new(color))),
-        "dumb_random" => Some(Box::new(DumbRandom::new(color))),
-        _ => panic!("unknown player label {}", label),
+/// A player choosen via command line.
+enum PlayerInput {
+    Human,
+    Other(Box<dyn Player>),
+}
+
+impl PlayerInput {
+    /// Returns the player or `None` if it's a human player.
+    fn computer_player(self) -> Option<Box<dyn Player>> {
+        match self {
+            PlayerInput::Human => None,
+            PlayerInput::Other(p) => Some(p),
+        }
+    }
+
+    fn from_str(input: &str, color: EdgeState) -> Result<Self, String> {
+        match input {
+            "human" => Ok(PlayerInput::Human),
+            "random" => Ok(PlayerInput::Other(Box::new(Random::new(color)))),
+            "dumb_random" => Ok(PlayerInput::Other(Box::new(DumbRandom::new(color)))),
+            _ => Err(format!(
+                "invalid player '{}' (valid options: 'human', 'random', 'dumb_random')",
+                input,
+            )),
+        }
     }
 }
 
+fn parse_player_red(input: &str) -> Result<PlayerInput, String> {
+    PlayerInput::from_str(input, EdgeState::Red)
+}
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "sim", about = "Implementation of the Sim pencil game")]
+fn parse_player_blue(input: &str) -> Result<PlayerInput, String> {
+    PlayerInput::from_str(input, EdgeState::Blue)
+}
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "sim",
+    about = "Implementation of the Sim pencil game",
+    usage = "sim [FLAGS] <player_red> <player_blue>",
+)]
 struct Opt {
-    /// The player with color red (the starting player).
-    #[structopt(default_value = "human")]
-    player_red: String,
+    /// The player with color red (the starting player). Possible values:
+    /// 'human', 'random', 'random_dumb'.
+    #[structopt(default_value = "human", parse(try_from_str = "parse_player_red"))]
+    player_red: PlayerInput,
 
-    /// The player with color blue
-    #[structopt(default_value = "random")]
-    player_blue: String,
+    /// The player with color blue.
+    #[structopt(default_value = "random", parse(try_from_str = "parse_player_blue"))]
+    player_blue: PlayerInput,
 }
